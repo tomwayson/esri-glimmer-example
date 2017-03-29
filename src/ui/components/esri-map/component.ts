bd826980610a3715cb18f05673de85a17bbfbf43
Tom Wayson <tom@tomwayson.com>
2017-03-28 21:17:54 -0700

    using local copy of esri-loader

--- src/ui/components/esri-map/component.ts
+++ src/ui/components/esri-map/component.ts
@@ -1,23 +1,46 @@
 import Component from '@glimmer/component';
+// TODO: once we figure out how to install esri-loader as an npm package
+// replace the following:
+import {isLoaded, bootstrap, dojoRequire} from './esri-loader';
+// with:
+// import {isLoaded, bootstrap, dojoRequire} from 'esri-loader';
 
 export default class EsriMap extends Component {
     didInsertElement() {
-        setTimeout(() => {
-            require(['esri/WebMap', 'esri/views/MapView'], (WebMap, MapView) =>{
-                const webmap = new WebMap({
-                    portalItem: { // autocasts as new PortalItem()
-                        id: this.args.itemId
-                    }
-                });
-
-                /************************************************************
-                * Set the WebMap instance to the map property in a MapView.
-                ************************************************************/
-                const view = new MapView({
-                    map: webmap,
-                    container: 'map'
-                });
+        // has the ArcGIS API been added to the page?
+        if (!isLoaded()) {
+            // no, lazy load it the ArcGIS API before using its classes
+            bootstrap((err) => {
+                if (err) {
+                    console.error(err);
+                } else {
+                    // once it's loaded, create the map
+                    this.createMap();
+                }
+            }, {
+                // use a specific version instead of latest 4.x
+                url: 'https://js.arcgis.com/4.3/'
             });
-        }, 500);
+        } else {
+            // ArcGIS API is already loaded, just create the map
+            this.createMap();
+        }
+    }
+
+    // create a map on the page
+    createMap() {
+        // first, we use Dojo's loader to require the map class
+        dojoRequire(['esri/WebMap', 'esri/views/MapView'], (WebMap, MapView) =>{
+            const webmap = new WebMap({
+                portalItem: { // autocasts as new PortalItem()
+                    id: (<any> this.args).itemId
+                }
+            });
+
+            const view = new MapView({
+                map: webmap,
+                container: 'map'
+            });
+        });
     }
 };
